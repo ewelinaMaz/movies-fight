8dd1a6a3c0dc47c4adc084469cf538bf904f0a04
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2021-03-15 12:34:39 +0100

    Start working on input

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '87615f27',
+            s: searchTerm
+        }
+    });
+
+};
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+})
