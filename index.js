f192568b59e96f6c9f745b5fdf3cd17503a86674
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2021-04-14 10:34:36 +0200

    Refactor autocomplete to reusable file

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "87615f27",
-      s: searchTerm,
-    },
-  });
 
-  if (response.data.Error) {
-    return [];
-  }
-  return response.data.Search;
-};
 
 createAutoComplete({
-   root: document.querySelector(".autocomplete")
+   root: document.querySelector(".autocomplete"),
+   renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+    <img src="${imgSrc}"/>
+    ${movie.Title} (${movie.Year})
+    `;
+   },
+   onOptionSelect(movie) {
+     onMovieSelect(movie)
+   },
+   inputValue(movie) {
+     return movie.Title;
+   },
+   async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "87615f27",
+        s: searchTerm,
+      },
+    });
+  
+    if (response.data.Error) {
+      return [];
+    }
+    return response.data.Search;
+  },
 });
 
 
